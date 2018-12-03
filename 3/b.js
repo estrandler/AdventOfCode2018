104f7a98f98a978f172bcdd7744b33eb79071107
Erik Strandler <erik@strandler.se>
2018-12-03 18:56:55 +0100

    Solved day 3

---
+++ 3/b.js
@@ -0,0 +1,49 @@
+var fs = require('fs');
+
+fs.readFile("./3/3.txt", "utf8", function (error, data) {
+    if (error) {
+        console.log(error);
+    }
+
+    var map = {};
+
+    var values = data.split('\r\n').map(val => {
+        return {
+            id: val.split(' ')[0],
+            x: parseInt(val.split(' ')[2].split(',')[0], 10),
+            y: parseInt(val.split(' ')[2].split(',')[1].replace(':', ''), 10),
+            width: parseInt(val.split(' ')[3].split('x')[0], 10),
+            height: parseInt(val.split(' ')[3].split('x')[1], 10)
+        }
+    });
+    
+    //Populare object
+    values.forEach((val) => {
+        for (let i = val.x; i < val.x + val.width; i++) {
+            for (let j = val.y; j < val.y + val.height; j++) {
+                let coords = i + ',' + j;
+
+                if (map[coords])
+                    map[coords] = 'x';
+                else
+                    map[coords] = coords;
+            }
+        }
+    });
+
+    values.forEach((val) => {
+        let noCollisions = true;
+        for (let i = val.x; i < val.x + val.width; i++) {
+            for (let j = val.y; j < val.y + val.height; j++) {
+                let coords = i + ',' + j;
+
+                if(map[coords] === 'x')
+                    noCollisions = false;
+            }
+        }
+
+        if(noCollisions)
+            console.log(val.id);
+    })
+
+});
